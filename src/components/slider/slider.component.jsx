f175ecc4875e4e292e51aea49b85487472c8426d
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-04-01 01:06:00 +0700

    feat: created slider component

--- src/components/slider/slider.component.jsx
+++ src/components/slider/slider.component.jsx
@@ -1,40 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import sliderData from "./slider-data";
 import "./slider.styles.scss";
 
-const endpoint = process.env.PUBLIC_URL;
-const imageUrl = endpoint + `/assets/images/sample1.jpeg`;
-
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const moveToNextSlide = () => {
+    if (slideIndex !== sliderData.length - 1) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === sliderData.length - 1) {
+      setSlideIndex(0);
+    }
+  };
+
+  const moveToPreviousSlide = () => {
+    if (slideIndex !== 0) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 0) {
+      setSlideIndex(sliderData.length - 1);
+    }
+  };
+
+  const moveBullet = (index) => {
+    setSlideIndex(index);
+  };
+
   return (
     <div className="container">
       <div
-        className="slider-container image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
+        className={
+          slideIndex === slideIndex + 1
+            ? "slider-container image image-animation"
+            : "slider-container image"
+        }
+        style={{ backgroundImage: `url(${sliderData[slideIndex].image})` }}
       ></div>
       <div className="slider-container review">
         <div>
           <h1>Reviews</h1>
           <div className="border-bottom"></div>
-          <p className="review-title">Best PRICE</p>
+          <p className="review-title">{sliderData[slideIndex].title}</p>
           <span className="quotation-symbol">&#8221;</span>
         </div>
         <div className="review-wrapper">
-          <span>&#9664;</span>
+          <span onClick={moveToPreviousSlide}>&#9664;</span>
           <div className="review">
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-            <p className="reviewer">- Peter Amar</p>
+            <p>{sliderData[slideIndex].review}</p>
+            <p className="reviewer">- {sliderData[slideIndex].reviewer}</p>
           </div>
-          <span>&#9654;</span>
+          <span onClick={moveToNextSlide}>&#9654;</span>
         </div>
         <div className="bullets-wrapper">
-          <div className="bullets selected"></div>
-          <div className="bullets"></div>
-          <div className="bullets"></div>
+          {sliderData.map((item, index) => (
+            <div
+              key={index}
+              className={slideIndex === index ? "bullets selected" : "bullets"}
+              onClick={() => moveBullet(index)}
+            ></div>
+          ))}
         </div>
       </div>
     </div>
